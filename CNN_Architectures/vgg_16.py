d43b656d1eb7372cf8eb9f5c23b1b6a0d1b17ad1
Hari Vidharth <viju1145@gmail.com>
2022-02-17 12:41:03 +0100

    Adding VGG16 Architecture

--- CNN_Architectures/vgg_16.py
+++ CNN_Architectures/vgg_16.py
@@ -10,59 +10,61 @@ __status__ = "Prototype"
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
+from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.models import Model
 
 
-class VGG16(Model):
-	def __init__(self, classes):
-		super(VGG16, self).__init__()
+class VGG16():
+	def __init__(self, x, y, z, o):
+		self.x = x
+		self. y = y
+		self.z = z
+		self.o = o
 
-		self.conv_1 = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_1")
-		self.conv_2 = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_2")
-		self.max_pool_1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="max_pool_1")
-		self.conv_3 = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_3")
-		self.conv_4 = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_4")
-		self.max_pool_2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="max_pool_2")
-		self.conv_5 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_5")
-		self.conv_6 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_6")
-		self.conv_7 = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_7")
-		self.max_pool_3 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="max_pool_3")
-		self.conv_8 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_8")
-		self.conv_9 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_9")
-		self.conv_10 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_10")
-		self.max_pool_4 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="max_pool_4")
-		self.conv_11 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_11")
-		self.conv_12 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_12")
-		self.conv_13 = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu", name="conv_13")
-		self.max_pool_5 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name="max_pool_5")
-		self.flatten = Flatten()
-		self.fc_1 = Dense(units=4096, activation="relu", name="fc_1")
-		self.fc_2 = Dense(units=4096, activation="relu", name="fc_2")
-		self.fc_3 = Dense(units=classes, activation="softmax", name="output_fc_3")
+	def vgg16_module(self):
+		self.input = Input(shape=(self.x, self.y, self.z))
 
-	def call(self, inputs):
-		x = self.conv_1(inputs)
-		x = self.conv_2(x)
-		x = self.max_pool_1(x)
-		x = self.conv_3(x)
-		x = self.conv_4(x)
-		x = self.max_pool_2(x)
-		x = self.conv_5(x)
-		x = self.conv_6(x)
-		x = self.conv_7(x)
-		x = self.max_pool_3(x)
-		x = self.conv_8(x)
-		x = self.conv_9(x)
-		x = self.conv_10(x)
-		x = self.max_pool_4(x)
-		x = self.conv_11(x)
-		x = self.conv_12(x)
-		x = self.conv_13(x)
-		x = self.max_pool_5(x)
-		x = self.flatten(x)
-		x = self.fc_1(x)
-		x = self.fc_2(x)
-		x = self.fc_3(x)
+		x = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(self.input)
+		x = Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
 
-		return x
+		x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same")(x)
+
+		x = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+		x = Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+
+		x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same")(x)
+
+		x = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+		x = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+		x = Conv2D(filters=256, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+
+		x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same")(x)
+
+		x = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+		x = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+		x = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+
+		x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same")(x)
+
+		x = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+		x = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+		x = Conv2D(filters=512, kernel_size=(3, 3), strides=(1, 1), padding="same", activation="relu")(x)
+
+		x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding="same")(x)
+
+		x = Flatten()(x)
+
+		x = Dense(units=4096, activation="relu")(x)
+		x = Dense(units=4096, activation="relu")(x)
+
+		self.output = Dense(units=self.o, activation="softmax")(x)
+
+	def vgg16_build(self):
+		self.vgg16_module()
+
+		network = Model(inputs=self.input, outputs=self.output)
+
+		network.summary()
+
+		return network
